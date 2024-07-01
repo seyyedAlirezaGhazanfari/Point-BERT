3e1d9ea1afa083110c84aa3c6e4fc155d5ed9341
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2024-07-01 11:12:54 +0330

    revert dataset

--- datasets/ModelNetDataset.py
+++ datasets/ModelNetDataset.py
@@ -16,32 +16,6 @@ import torch
 
 warnings.filterwarnings('ignore')
 
-# data augmentation start
-
-def random_scale_point_cloud(point_cloud, scale_low=0.8, scale_high=1.25):
-    scale = np.random.uniform(scale_low, scale_high)
-    point_cloud[:, 0:3] *= scale
-    return point_cloud
-
-def random_rotate_point_cloud(point_cloud):
-    theta = np.random.uniform(0, 2 * np.pi)
-    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
-                                [np.sin(theta), np.cos(theta), 0],
-                                [0, 0, 1]])
-    point_cloud[:, 0:3] = point_cloud[:, 0:3].dot(rotation_matrix)
-    return point_cloud
-
-def jitter_point_cloud(point_cloud, sigma=0.01, clip=0.05):
-    jitter = np.clip(sigma * np.random.randn(*point_cloud[:, 0:3].shape), -clip, clip)
-    point_cloud[:, 0:3] += jitter
-    return point_cloud
-
-def translate_point_cloud(point_cloud, shift_range=0.1):
-    shift = np.random.uniform(-shift_range, shift_range, 3)
-    point_cloud[:, 0:3] += shift
-    return point_cloud
-
-# data augmentation end
 
 def pc_normalize(pc):
     centroid = np.mean(pc, axis=0)
@@ -84,8 +58,8 @@ class ModelNet(Dataset):
         self.num_category = config.NUM_CATEGORY
         self.process_data = True
         self.uniform = True
-        self.subset = config.subset  # Correct assignment of subset
-        split = self.subset  # Correct assignment of split
+        split = config.subset
+        self.subset = config.subset
 
         if self.num_category == 10:
             self.catfile = os.path.join(self.root, 'modelnet10_shape_names.txt')
@@ -107,7 +81,7 @@ class ModelNet(Dataset):
         shape_names = ['_'.join(x.split('_')[0:-1]) for x in shape_ids[split]]
         self.datapath = [(shape_names[i], os.path.join(self.root, shape_names[i], shape_ids[split][i]) + '.txt') for i
                          in range(len(shape_ids[split]))]
-        print_log('The size of %s data is %d' % (split, len(self.datapath)), logger='ModelNet')
+        print_log('The size of %s data is %d' % (split, len(self.datapath)), logger = 'ModelNet')
 
         if self.uniform:
             self.save_path = os.path.join(self.root, 'modelnet%d_%s_%dpts_fps.dat' % (self.num_category, split, self.npoints))
@@ -116,7 +90,7 @@ class ModelNet(Dataset):
 
         if self.process_data:
             if not os.path.exists(self.save_path):
-                print_log('Processing data %s (only running in the first time)...' % self.save_path, logger='ModelNet')
+                print_log('Processing data %s (only running in the first time)...' % self.save_path, logger = 'ModelNet')
                 self.list_of_points = [None] * len(self.datapath)
                 self.list_of_labels = [None] * len(self.datapath)
 
@@ -137,7 +111,7 @@ class ModelNet(Dataset):
                 with open(self.save_path, 'wb') as f:
                     pickle.dump([self.list_of_points, self.list_of_labels], f)
             else:
-                print_log('Load processed data from %s...' % self.save_path, logger='ModelNet')
+                print_log('Load processed data from %s...' % self.save_path, logger = 'ModelNet')
                 with open(self.save_path, 'rb') as f:
                     self.list_of_points, self.list_of_labels = pickle.load(f)
 
@@ -157,23 +131,19 @@ class ModelNet(Dataset):
                 point_set = farthest_point_sample(point_set, self.npoints)
             else:
                 point_set = point_set[0:self.npoints, :]
-
+                
         point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
         if not self.use_normals:
             point_set = point_set[:, 0:3]
 
         return point_set, label[0]
 
+
     def __getitem__(self, index):
         points, label = self._get_item(index)
         pt_idxs = np.arange(0, points.shape[0])   # 2048
         if self.subset == 'train':
             np.random.shuffle(pt_idxs)
-            # Apply data augmentation
-            points = random_scale_point_cloud(points)
-            points = random_rotate_point_cloud(points)
-            points = jitter_point_cloud(points)
-            points = translate_point_cloud(points)
         current_points = points[pt_idxs].copy()
         current_points = torch.from_numpy(current_points).float()
         return 'ModelNet', 'sample', (current_points, label)
